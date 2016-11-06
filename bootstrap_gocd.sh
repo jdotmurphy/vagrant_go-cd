c4b75571d88e22fa1bfab8c6eb28ee22a1e1c0fc
John Murphy <John Murphy>
2016-11-06 11:45:51 -0800

    adding vagrantfile and shell script

---
+++ bootstrap_gocd.sh
@@ -0,0 +1,4 @@
+echo "deb https://download.go.cd /" | sudo tee /etc/apt/sources.list.d/gocd.list
+curl https://download.go.cd/GOCD-GPG-KEY.asc | sudo apt-key add -
+sudo apt-get update
+sudo apt-get -y install go-server
